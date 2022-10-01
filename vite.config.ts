25317dd6629f4c9fc2d7676a13386bc8c6e7d868
Matthew chen <Matgenius04@users.noreply.github.com>
2022-10-01 17:55:35 -0500

    fixing build config for prod

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    base: '/overlap-graph-generator/',
+    build: {
+        rollupOptions: {
+            // https://rollupjs.org/guide/en/#big-list-of-options
+        }
+    }
+});
